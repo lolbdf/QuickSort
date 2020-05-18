328f875f733b596716611b118e87e14156f7e450
lolbdf <62429100+lolbdf@users.noreply.github.com>
2020-05-18 14:40:10 +0200

    Das ist ein test

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import time
 import liste_mit_anzahl_der_wörter
 import exel
 
-# Das ist ein Test
+# 2. versuch
 
 if __name__ == "__main__":  # der teil wird nur ausgeführt wenn man die datei direkt ausführt nicht aber wenn man die Datei importiert
 
