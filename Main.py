1862c414ba0dc32f260c0044affdb6818bd18a0d
lolbdf <62429100+lolbdf@users.noreply.github.com>
2020-05-18 14:42:49 +0200

    Das ist ein test

--- Main.py
+++ Main.py
@@ -4,7 +4,8 @@ import time
 import liste_mit_anzahl_der_wörter
 import exel
 
-#neuer versuch
+#egalllll
+
 if __name__ == "__main__":  # der teil wird nur ausgeführt wenn man die datei direkt ausführt nicht aber wenn man die Datei importiert
 
     untergrenze = ""
