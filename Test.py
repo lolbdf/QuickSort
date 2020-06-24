921d8dee8d0b8c894f14cb58e3f9a972e19f873d
lolbdf <62429100+lolbdf@users.noreply.github.com>
2020-06-24 14:33:49 +0200

    comit

--- Test.py
+++ Test.py
@@ -1,6 +1,32 @@
-List = ["A", "Z", "a", "z"]
+def quicksort(L, anfang, ende):
+    if L != []:
+        pivot = L[anfang]
+        links = anfang
+        rechts = ende
+        while links <= rechts:
+            while L[links] < pivot:
+                links = links + 1
+            while L[rechts] > pivot:
+                rechts = rechts - 1
+            if links <= rechts:
+                if links < rechts:
+                    h = L[links]
+                    L[links] = L[rechts]
+                    L[rechts] = h
+                links = links + 1
+                rechts = rechts - 1
+            print(L)
+            print(anfang, ende, links, rechts)
+        print()
+        if anfang < rechts:
+            L = quicksort(L, anfang, rechts)
+        if links < ende:
+            L = quicksort(L, links, ende)
+    return L
 
 
-for buchstaben in List:
-
-    print(ord(buchstaben))
+# Test
+L = [25, 17, 32, 56, 25, 19, 8, 66, 29, 6, 20, 29]
+print(L)
+L = quicksort(L, 0, len(L) - 1)
+print(L)
