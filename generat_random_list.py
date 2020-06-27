9bbd7db63fd20edd81a81bc855541da536a92aa7
lolbdf <62429100+lolbdf@users.noreply.github.com>
2020-06-27 22:47:29 +0200

    simplified generate random list

--- generat_random_list.py
+++ generat_random_list.py
@@ -3,19 +3,16 @@ import random
 
 def create(Wort_Anzahl):
     liste = []
+
     for i in range(Wort_Anzahl):
-        wortlänge = int(random.randint(5, 10))
-        wort_list = []
-        for j in range(wortlänge):
-            buchstabe_zahl = int(random.randint(0, 25))
-            buchstabe = chr(buchstabe_zahl + 97)
-            wort_list.append(buchstabe)
-            wort = ""
-        for k in range(len(wort_list)):
-            groß = random.randint(0, 1)
-            if k == 0:
-                if groß == 1:
-                    wort_list[k] = chr(ord(wort_list[k]) - 32)
-            wort = wort + wort_list[k]
+        wortlaenge = int(random.randint(5, 10))
+        wort = ""
+        for j in range(wortlaenge):
+            buchstabe = chr(int(random.randint(97, 122)))
+            if j == 0:
+                grossbuchstabe = random.randint(0, 1)
+                if grossbuchstabe == 1:
+                    buchstabe = chr(ord(buchstabe) - 32)
+            wort = wort + buchstabe
         liste.append(wort)
     return liste
